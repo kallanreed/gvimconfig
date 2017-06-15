bd683be06cd7bc9322d982777579b8f935da4041
Kyle Reed <kylereed@microsoft.com>
2017-06-15 13:47:00 -0700

    Helper script to install files

---
+++ Install.ps1
@@ -0,0 +1,17 @@
+Write-Host "Installing VIM files..."
+
+$temp = "$pwd\vim_temp.zip"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile("https://github.com/kallanreed/gvimconfig/archive/master.zip", $temp)
+$shell = New-Object -ComObject Shell.Application
+$zip = $shell.Namespace("$temp\gvimconfig-master")
+
+foreach ($item in $zip.Items())
+{
+  if ($item.Name -eq "Readme.md") { continue }
+
+  Write-Host "Copying $($item.Name) to $env:USERPROFILE"
+  $shell.Namespace("$env:USERPROFILE").CopyHere($item)
+}
+
+Remove-Item $temp
